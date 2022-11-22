fbb957623016a9793ebc29d821ceeaff882cc47e
JuanGuillermoLauraMamani <jguillermolaura@gmail.com>
2022-11-22 19:27:43 -0400

    Updates

---
+++ precache-manifest.de912b74160ceab706acdc490c70095e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879ab8b9c038d060fb90cf6c88c3faa9",
+    "url": "/migrainefront/index.html"
+  },
+  {
+    "revision": "23ab8b69ed79adcb8e14",
+    "url": "/migrainefront/static/css/2.c1c45ef1.chunk.css"
+  },
+  {
+    "revision": "e365ea22e78a03e6bfe6",
+    "url": "/migrainefront/static/css/main.904b0e6c.chunk.css"
+  },
+  {
+    "revision": "23ab8b69ed79adcb8e14",
+    "url": "/migrainefront/static/js/2.bbf8ba49.chunk.js"
+  },
+  {
+    "revision": "4595d64479c2a9393c1513ecf996de90",
+    "url": "/migrainefront/static/js/2.bbf8ba49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e365ea22e78a03e6bfe6",
+    "url": "/migrainefront/static/js/main.6591443b.chunk.js"
+  },
+  {
+    "revision": "27a093f3c0f8796da4a5",
+    "url": "/migrainefront/static/js/runtime-main.b9f36426.js"
+  },
+  {
+    "revision": "52ecff6cbe03ca614889389eea209698",
+    "url": "/migrainefront/static/media/auth.52ecff6c.svg"
+  },
+  {
+    "revision": "bad19771db42d9c212abd5da4fe7a827",
+    "url": "/migrainefront/static/media/login.bad19771.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ebed07e44d3233fd82d5f1ef2ac279db.js"
+  "/migrainefront/precache-manifest.de912b74160ceab706acdc490c70095e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/migrainefront/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
