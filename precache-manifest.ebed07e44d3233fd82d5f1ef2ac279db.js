fc4b0a70e756e6d86411e2e9ed8b4cb476e3b7e4
JuanGuillermoLauraMamani <jguillermolaura@gmail.com>
2022-11-22 19:08:55 -0400

    Updates

---
+++ precache-manifest.ebed07e44d3233fd82d5f1ef2ac279db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7415247389cc4e4ff533148570f83bda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00fd215457eb1b7fadde",
+    "url": "/static/css/2.c1c45ef1.chunk.css"
+  },
+  {
+    "revision": "c455441d58413dd3e351",
+    "url": "/static/css/main.904b0e6c.chunk.css"
+  },
+  {
+    "revision": "00fd215457eb1b7fadde",
+    "url": "/static/js/2.f82e23a0.chunk.js"
+  },
+  {
+    "revision": "4595d64479c2a9393c1513ecf996de90",
+    "url": "/static/js/2.f82e23a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c455441d58413dd3e351",
+    "url": "/static/js/main.5e93267a.chunk.js"
+  },
+  {
+    "revision": "940b9cb0c27bb71a470a",
+    "url": "/static/js/runtime-main.fc5eeba3.js"
+  },
+  {
+    "revision": "52ecff6cbe03ca614889389eea209698",
+    "url": "/static/media/auth.52ecff6c.svg"
+  },
+  {
+    "revision": "bad19771db42d9c212abd5da4fe7a827",
+    "url": "/static/media/login.bad19771.svg"
+  }
+]);
